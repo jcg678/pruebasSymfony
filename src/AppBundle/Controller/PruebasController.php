5d75e4b898103a84fa5972e1a91d4200c6126aef
jcg678 <jcg678@hotmail.es>
2016-11-29 22:54:21 +0100

    redireciones 21

--- src/AppBundle/Controller/PruebasController.php
+++ src/AppBundle/Controller/PruebasController.php
@@ -11,6 +11,10 @@ class PruebasController extends Controller
     
     public function indexAction(Request $request,$name,$page)
     {
+        //return $this->redirect($this->generateUrl("homepage"));
+        //return $this->redirect($this->container->get("router")->getContext()->getBaseUrl()."/hello-world?prueba=true");
+        return $this->redirect($request->getBaseUrl()."/hello-world?prueba=true");
+        
         // replace this example code with whatever you need
         return $this->render('AppBundle:pruebas:index.html.twig', [
             'texto' => $name." - ".$page
