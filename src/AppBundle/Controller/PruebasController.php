6296f45e7d39d8c79d048d4e217bdc2783c5d1e7
jcg678 <jcg678@hotmail.es>
2016-11-29 23:01:47 +0100

    obtener parametros url

--- src/AppBundle/Controller/PruebasController.php
+++ src/AppBundle/Controller/PruebasController.php
@@ -13,7 +13,12 @@ class PruebasController extends Controller
     {
         //return $this->redirect($this->generateUrl("homepage"));
         //return $this->redirect($this->container->get("router")->getContext()->getBaseUrl()."/hello-world?prueba=true");
-        return $this->redirect($request->getBaseUrl()."/hello-world?prueba=true");
+        //return $this->redirect($request->getBaseUrl()."/hello-world?prueba=true");
+        // para get
+        var_dump($request->query->get("hola"));
+        // en caso de post
+        var_dump($request->get("hola-post"));
+        die();
         
         // replace this example code with whatever you need
         return $this->render('AppBundle:pruebas:index.html.twig', [
