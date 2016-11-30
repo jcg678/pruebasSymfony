fe4e047bb3334bb377e6259870f89a2ceaabd4af
jcg678 <jcg678@hotmail.es>
2016-11-30 17:34:40 +0100

    twig condicionales bucles start end

--- src/AppBundle/Controller/PruebasController.php
+++ src/AppBundle/Controller/PruebasController.php
@@ -11,18 +11,27 @@ class PruebasController extends Controller
     
     public function indexAction(Request $request,$name,$page)
     {
-        //return $this->redirect($this->generateUrl("homepage"));
+       
         //return $this->redirect($this->container->get("router")->getContext()->getBaseUrl()."/hello-world?prueba=true");
-        //return $this->redirect($request->getBaseUrl()."/hello-world?prueba=true");
-        // para get
-        //var_dump($request->query->get("hola"));
-        // en caso de post
-        //var_dump($request->get("hola-post"));
-        //die();
+        
+        $productos = array(
+                    array("producto"=>"Consola 1","precio"=>2),
+                    array("producto"=>"Consola 2","precio"=>23),
+                    array("producto"=>"Consola 3","precio"=>24),
+                    array("producto"=>"Consola 4","precio"=>25),
+                    array("producto"=>"Consola 5","precio"=>26),
+                        );
+        $fruta=array(
+        "manzana"=>"golden",
+        "pera"=>"rica"
+        
+        );
         
         // replace this example code with whatever you need
         return $this->render('AppBundle:pruebas:index.html.twig', [
-            'texto' => $name." - ".$page
+            'texto' => $name." - ".$page,
+            'productos'=> $productos,
+            'fruta'=> $fruta
         ]);
     }
     
--- var/cache/dev/twig/98/988b5948e03d93f32f68488b1510cab2e0634a539d9135f6a5627dd67d3e0a08.php
+++ var/cache/dev/twig/98/988b5948e03d93f32f68488b1510cab2e0634a539d9135f6a5627dd67d3e0a08.php
@@ -21,20 +21,20 @@ class __TwigTemplate_cfb0c29a5fca23d34beb000ccc942072111429e661556c5b76d05286e4a
 
     protected function doDisplay(array $context, array $blocks = array())
     {
-        $__internal_9442fc02bf0a492684f09f7cef03e0ab8a997d60106047dd9683dd9497492a8b = $this->env->getExtension("native_profiler");
-        $__internal_9442fc02bf0a492684f09f7cef03e0ab8a997d60106047dd9683dd9497492a8b->enter($__internal_9442fc02bf0a492684f09f7cef03e0ab8a997d60106047dd9683dd9497492a8b_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "AppBundle:pruebas:index.html.twig"));
+        $__internal_e77275021dd4438db7b22025790b1d1bb362e36856c827e9397b1836028d003c = $this->env->getExtension("native_profiler");
+        $__internal_e77275021dd4438db7b22025790b1d1bb362e36856c827e9397b1836028d003c->enter($__internal_e77275021dd4438db7b22025790b1d1bb362e36856c827e9397b1836028d003c_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "AppBundle:pruebas:index.html.twig"));
 
         $this->parent->display($context, array_merge($this->blocks, $blocks));
         
-        $__internal_9442fc02bf0a492684f09f7cef03e0ab8a997d60106047dd9683dd9497492a8b->leave($__internal_9442fc02bf0a492684f09f7cef03e0ab8a997d60106047dd9683dd9497492a8b_prof);
+        $__internal_e77275021dd4438db7b22025790b1d1bb362e36856c827e9397b1836028d003c->leave($__internal_e77275021dd4438db7b22025790b1d1bb362e36856c827e9397b1836028d003c_prof);
 
     }
 
     // line 2
     public function block_container($context, array $blocks = array())
     {
-        $__internal_d7ccba3a8f6bc1700ec2051b6da58c709e811dababdc05acdb7ada0496676925 = $this->env->getExtension("native_profiler");
-        $__internal_d7ccba3a8f6bc1700ec2051b6da58c709e811dababdc05acdb7ada0496676925->enter($__internal_d7ccba3a8f6bc1700ec2051b6da58c709e811dababdc05acdb7ada0496676925_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "container"));
+        $__internal_9b761ce939920f203d7a9a4d834c545d6f75b4e8b4e0ae4cc0cef6af7b26fefd = $this->env->getExtension("native_profiler");
+        $__internal_9b761ce939920f203d7a9a4d834c545d6f75b4e8b4e0ae4cc0cef6af7b26fefd->enter($__internal_9b761ce939920f203d7a9a4d834c545d6f75b4e8b4e0ae4cc0cef6af7b26fefd_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "container"));
 
         // line 3
         echo "    ";
@@ -44,9 +44,69 @@ class __TwigTemplate_cfb0c29a5fca23d34beb000ccc942072111429e661556c5b76d05286e4a
         // line 4
         echo twig_escape_filter($this->env, (isset($context["texto"]) ? $context["texto"] : $this->getContext($context, "texto")), "html", null, true);
         echo "
-";
+    <hr>
+    ";
+        // line 6
+        echo twig_escape_filter($this->env, $this->getAttribute((isset($context["fruta"]) ? $context["fruta"] : $this->getContext($context, "fruta")), "manzana", array()), "html", null, true);
+        echo "
+    ";
+        // line 7
+        if ((is_string($__internal_5c74acae640731b613286b518517228fa4662fc8d94382b4ebb2abe58c27694a = $this->getAttribute((isset($context["fruta"]) ? $context["fruta"] : $this->getContext($context, "fruta")), "pera", array())) && is_string($__internal_6c41471568a29ce2c07cd1b1b90c2de810bab1efb805ddb2c1db61f8c9b8d9d0 = "r") && ('' === $__internal_6c41471568a29ce2c07cd1b1b90c2de810bab1efb805ddb2c1db61f8c9b8d9d0 || 0 === strpos($__internal_5c74acae640731b613286b518517228fa4662fc8d94382b4ebb2abe58c27694a, $__internal_6c41471568a29ce2c07cd1b1b90c2de810bab1efb805ddb2c1db61f8c9b8d9d0)))) {
+            // line 8
+            echo "    ";
+            echo twig_escape_filter($this->env, $this->getAttribute((isset($context["fruta"]) ? $context["fruta"] : $this->getContext($context, "fruta")), "manzana", array()), "html", null, true);
+            echo "
+    ";
+        }
+        // line 10
+        echo "    ";
+        if ((is_string($__internal_4d6f51d269cf2daac8c8d7504d43ff18200575dd75057f3c77b6e26f25a4a350 = $this->getAttribute((isset($context["fruta"]) ? $context["fruta"] : $this->getContext($context, "fruta")), "pera", array())) && is_string($__internal_d5f73400412b4fe453f6fe6a78838329494dd9d6dc00b6a5860c8470afe56a65 = "a") && ('' === $__internal_d5f73400412b4fe453f6fe6a78838329494dd9d6dc00b6a5860c8470afe56a65 || $__internal_d5f73400412b4fe453f6fe6a78838329494dd9d6dc00b6a5860c8470afe56a65 === substr($__internal_4d6f51d269cf2daac8c8d7504d43ff18200575dd75057f3c77b6e26f25a4a350, -strlen($__internal_d5f73400412b4fe453f6fe6a78838329494dd9d6dc00b6a5860c8470afe56a65))))) {
+            // line 11
+            echo "    ";
+            echo twig_escape_filter($this->env, $this->getAttribute((isset($context["fruta"]) ? $context["fruta"] : $this->getContext($context, "fruta")), "manzana", array()), "html", null, true);
+            echo "
+    ";
+        }
+        // line 13
+        echo "    
+   
+    ";
+        // line 15
+        $context["variable"] = "Hola Twig";
+        // line 16
+        echo "    <hr>
+    ";
+        // line 17
+        echo twig_escape_filter($this->env, (isset($context["variable"]) ? $context["variable"] : $this->getContext($context, "variable")), "html", null, true);
+        echo "
+    <hr>
+    ";
+        // line 19
+        if ((twig_length_filter($this->env, (isset($context["productos"]) ? $context["productos"] : $this->getContext($context, "productos"))) > 0)) {
+            // line 20
+            echo "    <ul>
+    ";
+            // line 21
+            $context['_parent'] = $context;
+            $context['_seq'] = twig_ensure_traversable((isset($context["productos"]) ? $context["productos"] : $this->getContext($context, "productos")));
+            foreach ($context['_seq'] as $context["_key"] => $context["producto"]) {
+                // line 22
+                echo "     <li>";
+                echo twig_escape_filter($this->env, $this->getAttribute($context["producto"], "producto", array()), "html", null, true);
+                echo "-";
+                echo twig_escape_filter($this->env, $this->getAttribute($context["producto"], "precio", array()), "html", null, true);
+                echo "</li>
+    ";
+            }
+            $_parent = $context['_parent'];
+            unset($context['_seq'], $context['_iterated'], $context['_key'], $context['producto'], $context['_parent'], $context['loop']);
+            $context = array_intersect_key($context, $_parent) + $_parent;
+            // line 24
+            echo "    </ul>
+    ";
+        }
         
-        $__internal_d7ccba3a8f6bc1700ec2051b6da58c709e811dababdc05acdb7ada0496676925->leave($__internal_d7ccba3a8f6bc1700ec2051b6da58c709e811dababdc05acdb7ada0496676925_prof);
+        $__internal_9b761ce939920f203d7a9a4d834c545d6f75b4e8b4e0ae4cc0cef6af7b26fefd->leave($__internal_9b761ce939920f203d7a9a4d834c545d6f75b4e8b4e0ae4cc0cef6af7b26fefd_prof);
 
     }
 
@@ -62,11 +122,32 @@ class __TwigTemplate_cfb0c29a5fca23d34beb000ccc942072111429e661556c5b76d05286e4a
 
     public function getDebugInfo()
     {
-        return array (  45 => 4,  40 => 3,  34 => 2,  11 => 1,);
+        return array (  105 => 24,  94 => 22,  90 => 21,  87 => 20,  85 => 19,  80 => 17,  77 => 16,  75 => 15,  71 => 13,  65 => 11,  62 => 10,  56 => 8,  54 => 7,  50 => 6,  45 => 4,  40 => 3,  34 => 2,  11 => 1,);
     }
 }
 /* {% extends "::layout.html.twig" %}*/
 /* {% block container %}*/
 /*     {{parent()}}*/
 /*     {{texto}}*/
+/*     <hr>*/
+/*     {{fruta.manzana}}*/
+/*     {% if fruta.pera starts with "r" %}*/
+/*     {{fruta.manzana}}*/
+/*     {% endif %}*/
+/*     {% if fruta.pera ends with "a" %}*/
+/*     {{fruta.manzana}}*/
+/*     {% endif %}*/
+/*     */
+/*    */
+/*     {% set variable = "Hola Twig"%}*/
+/*     <hr>*/
+/*     {{variable}}*/
+/*     <hr>*/
+/*     {% if productos|length > 0  %}*/
+/*     <ul>*/
+/*     {% for producto in productos %}*/
+/*      <li>{{ producto.producto }}-{{producto.precio}}</li>*/
+/*     {% endfor %}*/
+/*     </ul>*/
+/*     {% endif %}*/
 /* {% endblock %}*/
