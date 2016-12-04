49dccd8266939d234ee904d2ea2a48ef5da3999b
jcg678 <jcg678@hotmail.es>
2016-12-04 16:28:35 +0100

    helper demo terminado generando tabla con array

--- src/AppBundle/Twig/HelperVistas.php
+++ src/AppBundle/Twig/HelperVistas.php
@@ -4,17 +4,19 @@ class HelperVistas extends \Twig_Extension{
     
     public function getFunctions(){
         return array(
-            'generateTable' => new \Twig_function_Method($this, 'generateTable')
+            'generateTable' => new \Twig_Function_Method($this, 'generateTable')
         );
        
     }
     
-    public function generateTable($num_columns,$num_rows){
-        $table="<table class='table'>";
-        for($i=0;$i<=$num_rows;$i++){
+    public function generateTable($array,$border){
+        $table="<table class='table' border=".$border." >";
+        for($i=0;$i<count($array);$i++){
             $table.="<tr>";
-            for($i=0;$i<=$num_columns; $i++){
-                $table.="<td>Columna</td>";
+            for($x=0;$x<count($array[$i]); $x++){
+                //convierte el array asociativo a valor
+                $array_res= array_values($array[$i]);
+                $table.="<td>".$array_res[$x]."</td>";
             }
             $table.="</tr>";
         }
