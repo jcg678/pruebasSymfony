ed3d23d43b27a8c2a0f3b28680f7bf00be664349
jcg678 <jcg678@hotmail.es>
2016-12-06 17:27:04 +0100

    filtro personalizado twig

---
+++ src/AppBundle/Twig/FilterVista.php
@@ -0,0 +1,20 @@
+<?php
+namespace AppBundle\Twig;
+class FilterVista extends \Twig_Extension{
+    
+    public function getFilters(){
+        return array(
+            new \Twig_SimpleFilter("addText", array($this, 'addText'))
+        );
+        
+    }
+    
+    public function addText($string,$num){
+        
+        return $string." Texto añadido: ".$num ;
+    }
+    
+    public function getName(){
+        return "filter_vista";
+    }
+}
