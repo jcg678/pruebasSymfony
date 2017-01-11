54bedc61335a889ec711cbd7e4d810a03a5997ac
jcg678 <jcg678@hotmail.es>
2017-01-11 22:52:06 +0100

    plantilla

--- src/BlogBundle/Controller/DefaultController.php
+++ src/BlogBundle/Controller/DefaultController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexOld()
     {
         /*
         $em = $this->getDoctrine()->getManager();
@@ -54,6 +54,10 @@ class DefaultController extends Controller
         };
 
         die();
+        return $this->render('BlogBundle:Default:index.html.twig');
+    }
+
+    public function indexAction(){
         return $this->render('BlogBundle:Default:index.html.twig');
     }
 }
