ab805af81cd61e53540cc071c52b7c8c1a917236
jcg678 <jcg678@hotmail.es>
2017-01-27 15:46:37 +0100

    etiquetas en menu

--- src/BlogBundle/Controller/EntryController.php
+++ src/BlogBundle/Controller/EntryController.php
@@ -20,9 +20,12 @@ class EntryController extends Controller
         $em = $this->getDoctrine()->getManager();
 
         $entry_repo=$em->getRepository("BlogBundle:Entry");
+        $category_repo= $em->getRepository("BlogBundle:Category");
         $entries=$entry_repo->findAll();
+        $categories=$category_repo->findAll();
         return $this->render("BlogBundle:entry:index.html.twig",[
-                'entries'=>$entries
+                'entries'=>$entries,
+                'categories'=>$categories
             ]
 
             );
